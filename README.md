271d45a56e26ec28c8f7a036d6e1eb400c2405a3
victoriaren2024 <vren4@ucsc.edu>
2026-02-28 03:27:04 -0800

    updated README, found issue with image paths for subset csv

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # ParkingSpotCounter
 Convolutional neural network regression model that counts the number of parking spots available/occupied in an image.
+
+Due to the large size of the dataset, the images are not included in this repository.
+
+Download the dataset from: http://cnrpark.it/
--- model.py
+++ model.py
@@ -1,19 +1,30 @@
+import os
 import pandas as pd
 from PIL import Image
-import os
+import numpy as np
 
-# paths 
-# subset the data
+import torch
+from torch.utils.data import Dataset, DataLoader
+from torchvision.transforms import v2
+
 csv_path = "CNRPark_per_image.csv"
 img_root = "FULL_IMAGE_1000x750"
 
-# load + clean csv
 df = pd.read_csv(csv_path)
 
-#keep only rows we can use
-df = df.dropna(subset=["camera", "weather", "year", "month", "day", "hour", "minute", "num_cars"])
-df = df.drop_duplicates()
-df = df.sample(frac=1, random_state=42).reset_index(drop=True)
+df = df.dropna(subset=["image_url", "num_cars"]).drop_duplicates().reset_index(drop=True)
 
-print("Rows after cleaning:", len(df))
-print("Example row:\n", df.iloc[0])
+# inspect what image_url looks like
+print("example image_url:", df.loc[0, "image_url"])
+
+# test an image
+#img = Image.open(df.iloc[0]["img_path"])
+#print(img.size)
+#img.show()
+
+
+# NOTES: would we have to build a local path from image_url? TT
+
+# the example image_url from the subset csv gives us : example image_url: CNR-EXT/PATCHES/SUNNY/2015-11-12/camera1/S_2015-11-12_07.09_C01_184.jpg
+# but our actual images are in: FULL_IMAGE_1000x750/SUNNY/2015-11-12/camera1/...
+
